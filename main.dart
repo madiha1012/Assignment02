60df47e9d0f8739ec5703510fea022ce60695d7a
Madiha Khan <60982372+madiha1012@users.noreply.github.com>
2022-11-06 13:01:24 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,102 @@
+//import 'dart:html';
+//import 'dart:io';
+
+void main() {
+  /* print("Question no 01");
+  print("Enter your Length");
+  int? Length = int.parse(stdin.readLineSync()!);
+  print("length is $Length");
+  print("Enter your Breadth");
+  int? breadth = int.parse(stdin.readLineSync()!);
+  print("length is $breadth");
+  if (Length == breadth) {
+    print("It is Square");
+  } else {
+    print("It is Rectangle");
+  }
+  print("Question no 2");
+  print("Enter Age of Man1");
+  int? man1 = int.parse(stdin.readLineSync()!);
+  print("man1 is $man1");
+  print("Enter Age of man2");
+  int? man2 = int.parse(stdin.readLineSync()!);
+  print("man2 is $man2");
+  print("Enter Age of Man3");
+  int? man3 = int.parse(stdin.readLineSync()!);
+  print("man3 is $man3");
+  print("=============");
+  if (man1 >= man2 && man1 >= man3) {
+    print("man1 is oldest");
+  } else if (man2 >= man1 && man2 >= man3) {
+    print("man2 is oldest");
+  } else {
+    print("man3 is youngest");
+  }
+  print("==========");
+  if (man1 <= man3 && man1 <= man2) {
+    print("Man1 is youngest");
+  } else if (man2 <= man1 && man2 <= man3) {
+    print("man2 is youngest");
+  } else {
+    print("man3 is youngest");
+  }
+
+  print("Question no 03");
+  int number_of_classHeld = 16;
+  print("Number of Classes Held");
+  int number_of_classAttend = 10;
+  print("Number of Classes Attend by student");
+  var per = (number_of_classAttend * 100) / number_of_classHeld;
+  print("Student Attend $per % classes");
+  if (per <= 75) {
+    print("you are not allowed to sit in exam");
+  } else {
+    print("You are allowed to sit in exam");
+  }
+  print("QUestion no 04");
+  print("Enter Year");
+  int? year = int.parse(stdin.readLineSync()!);
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        print(" $year, is  a leap year");
+      } else {
+        print(" $year, is not a leap year");
+      }
+    } else {
+      print(" $year, is  a leap year");
+    }
+  } else {
+    print(" $year, is not a leap year");
+  }
+
+  print("Question : 05");
+  num temp = 42;
+  if (temp < 0) {
+    print("Freezing Weather");
+  } else if (temp < 10) {
+    print("Very Cold Weather");
+  } else if (temp < 20) {
+    print("Cold Weather");
+  } else if (temp < 30) {
+    print("Normal Temperature");
+  } else if (temp < 40) {
+    print("its hot");
+  } else if (temp >= 40) {
+    print("very hot");
+  }
+
+  print("Question No: 06");
+  var abc = "a";
+  print(abc);
+  if (abc == "a" &&
+      abc == "i" &&
+      abc == "e" &&
+      abc == "i" &&
+      abc == "o" &&
+      abc == "u") {
+    print("vowel");
+  } else {
+    print("not vowel");
+  }*/
+}
